375a177f29387c774f550aa8617d7a81667ef674
Сенгер Андрей Сергеевич, ИП-513 <sengerrid@gmail.com>
2025-10-07 10:18:31 +0700

    еще раз менял код в ./src/exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <locale.h>
 
-int main() {
-    double a[10], sum = 0;
-    for (int i = 0; i < 10; ++i) scanf("%lf", &a[i]);
-    for (int i = 0; i < 10; ++i) sum += a[i];
-    printf("%.2f\n", sum / 10.0);
+int main(void) {
+    setlocale(LC_NUMERIC, "C");
+    double a[10], sum = 0.0;
+    for (int i = 0; i < 10; ++i) {
+        if (scanf("%lf", &a[i]) != 1) return 1;
+        sum += a[i];
+    }
+    double avg = sum / 10.0;
+
+    char buf[64];
+    int n = snprintf(buf, sizeof(buf), "%.2f", avg);
+
+    while (n > 0 && buf[n - 1] == '0') buf[--n] = '\0';
+    if (n > 0 && buf[n - 1] == '.') buf[--n] = '\0';
+
+    printf("%s\n", buf);
     return 0;
 }
+
 
