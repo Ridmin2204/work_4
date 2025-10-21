0d77df3a0724790702aa1e7d3c55762d12429c49
Сенгер Андрей Сергеевич, ИП-513 <sengerrid@gmail.com>
2025-10-21 07:18:05 +0700

    Попытка-1_hw5

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,13 @@
-#include "../include/array_function.h"
+#include <stddef.h>
+#include "array_function.h"
+void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    for (size_t i = 0; i < size_src1; ++i) {
+        dest[i] = src1[i];
+    }
+    for (size_t j = 0; j < size_src2; ++j) {
+        dest[size_src1 + j] = src2[j];
+    }
+}
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
 
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
-
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
 
