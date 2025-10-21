53bd292ef3c189ba431c83aa6ae07d2dc0d90e35
Сенгер Андрей Сергеевич, ИП-513 <sengerrid@gmail.com>
2025-10-21 07:22:19 +0700

    Попытка-2_hw5

--- src/array_function.c
+++ src/array_function.c
@@ -9,5 +9,33 @@ void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, i
     }
 }
 
+void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    for (size_t i = 0; i < size_src1; ++i) {
+        dest[i] = src1[i];
+    }
+    for (size_t j = 0; j < size_src2; ++j) {
+        dest[size_src1 + j] = src2[j]
+    }
+}
+
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    size_t i = 0, j = 0, k = 0;
+    while (i < size_src1 && j < size_src2) {
+        if (src1[i] <= src2[j]) dest[k++] = src1[i++];
+        else                    dest[k++] = src2[j++];
+    }
+    while (i < size_src1) dest[k++] = src1[i++];
+    while (j < size_src2) dest[k++] = src2[j++];
+}
+
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+    int mn = src[0], mx = src[0];
+    for (size_t i = 1; i < size; ++i) {
+        if (src[i] < mn) mn = src[i];
+        if (src[i] > mx) mx = src[i];
+    }
+    *min = mn;
+    *max = mx;
+}
 
 
